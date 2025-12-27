42508cee554b92af95b518902a66a3c66ce3d99e
Than Hteik Aung <thanhtaikaung90@gmail.com>
2025-12-27 17:53:56 +0630

    remove shopping item -remove item form list when click on item -not allow to add same item -check empty input data

--- 2-js/ShoppingList.js
+++ 2-js/ShoppingList.js
@@ -1,28 +1,50 @@
 /* Declare variable names */
-const inputEl = document.querySelector("input");  //get the input element with id= item
-const formEl = document.querySelector("#myForm"); //get the form element wieh id= myForm
+const inputEl = document.querySelector("input");  //get the input element
+const formEl = document.querySelector("#myForm"); //get the form element with id= myForm
+const ulEl = document.querySelector("ul");        //get the ul element
+let arryItemList = new Array;
 
 /* myForm of submit eventlistener */
-formEl.addEventListener("submit",function(event){
+formEl.addEventListener("submit", function (event) {
     event.preventDefault();                       //prevent the default form submission (page reload)
-    addItem();                                    //call addItem()
+    addItemFun();                                    //call addItem()
     document.getElementById("myForm").reset();    //clear text inside of input txt box
 })
 
+/* li tag eventListener */
+ulEl.addEventListener("click", function (event) {
+    let removeItem;
+
+    event.preventDefault();
+    removeItem = event.target.textContent;
+    const lastItem = document.querySelector(`#${removeItem}`);
+    lastItem.remove();
+
+})
+
 /* add shopping item */
-function addItem(){
-    let inputItem; //input item name in this variable
-    
+function addItemFun() {
+    let inputItem;     //input item name in this variable
+    let deleSpaceItm;  //save vaule after remove whitespace
+
     inputItem = inputEl.value;
-    insertNewElement(inputItem);
+    deleSpaceItm = inputItem.trim();
+
+    /* not allow to add empty data and same shopping item */
+    if (deleSpaceItm.length !== 0 && arryItemList.includes(deleSpaceItm) === false) {
+        insertNewElement(inputItem);
+    }
 
 }
 
 /* create li tag inside of ul tag */
-function insertNewElement(item){
+function insertNewElement(item) {
     const newElItem = document.createElement('li');               //create li tag obj
     const parentNode = document.querySelector('#shoppingList');   //create ul tag obj
 
     newElItem.textContent = item;                                 //add item in new list
+    newElItem.id = item;                                          //setup id name
     parentNode.appendChild(newElItem);                            //append li tag with txt inside of ul tag
+    arryItemList.push(item);                                      //save item in array
+
 }
